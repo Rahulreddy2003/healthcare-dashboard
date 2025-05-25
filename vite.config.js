857ebea266bf2bfe61250d628956e3b80d8927e5
Rahulreddy2003 <122749010+Rahulreddy2003@users.noreply.github.com>
2025-05-25 10:19:41 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ Don't include a `base` unless deploying to GitHub Pages
+  base: './', // ✅ This is what Vercel needs to correctly resolve paths
 })
