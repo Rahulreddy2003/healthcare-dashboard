3554a488b3485f93886fa253011d9acc4f990059
Rahulreddy2003 <122749010+Rahulreddy2003@users.noreply.github.com>
2025-05-27 07:47:28 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/", // base must be "/"
   plugins: [react()],
-});
+  base: '/healthcare-dashboard/' // GitHub repo name here
+})
